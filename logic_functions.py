bf1ad56e8197f18030ac8597420b9348fcc9022b
kli512 <li.kevin512@gmail.com>
2019-10-12 11:24:43 -0700

    dev

---
+++ logic_functions.py
@@ -0,0 +1,11 @@
+def cell_danger(cell):
+    x, y = cell.position.x, cell.position.y
+    for pos_x in range(x - 5, x + 5):
+        for pos_y in range(y - 5, y + 5):
+            try:
+                if 
+
+    pass
+
+
+def
